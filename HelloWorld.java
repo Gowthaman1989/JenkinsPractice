23e86a2b5f8db6db11e536e2c9b0ebb829c740ef
Gowthaman1989 <s.gowthaman1989@gmail.com>
2023-03-11 11:22:39 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld{
 pubic static void main(String args[]){
 System.out.println("Hi, Hello {:-)} ");
 System.out.println("Have a nice day !");
-  System.out.println("Life is Short Spend it Happy!");
+ // System.out.println("Life is Short Spend it Happy!");
 
 }
 }
